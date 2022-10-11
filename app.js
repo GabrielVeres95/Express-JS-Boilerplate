63ff0c7fc043ba74fd1411b43e09406eaddced7f
Gabriel Veres <veresgabriel95@gmail.com>
2022-10-11 17:03:53 +0300

    Added more modules

--- app.js
+++ app.js
@@ -1,12 +1,34 @@
 require('dotenv').config()
 
+const logger = require('morgan')
 const express = require('express')
+const errorHandler = require('errorhandler')
+
+const uaParser = require('ua-parser-js')
+
 const app = express()
 const path = require('path')
 const port = 3000
 
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use(errorHandler())
+app.use(express.static(path.join(__dirname, 'public')))
+
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
+
+app.use((req, res, next) => {
+  // console.log(req.headers)
+  const ua = uaParser(req.headers['user-agent'])
+  // console.log(ua)
+  res.locals.isDesktop = ua.device.type === undefined
+  res.locals.isPhone = ua.device.type === 'mobile'
+  res.locals.isTablet = ua.device.type === 'tablet'
+  next()
+})
 
 app.get('/', (req, res) => {
   res.render('pages/home')
